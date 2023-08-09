043bc6cce6a46a90ac18d7167ac2f016bbb5a176
okanay <okanay@hotmail.com>
2023-08-09 18:49:55 +0300

    Categories Text Bug Fix

--- src/components/Categories-Page-Components/ProductsItems.tsx
+++ src/components/Categories-Page-Components/ProductsItems.tsx
@@ -7,7 +7,7 @@ type TProps = {
 
 export const ProductsItems = async ({ categories = 'beverages' }: TProps) => {
    return (
-      <div className={'mt-14 h-96 w-full px-6 py-2 baseTablet:mt-0 baseTablet:px-4'}>
+      <div className={'mt-12 h-96 w-full px-6 py-2 baseTablet:mt-0 baseTablet:px-4'}>
          <h1>{categories}</h1>
       </div>
    )
--- src/components/Categories-Page-Components/ProductsMenu/ProductsMenu.tsx
+++ src/components/Categories-Page-Components/ProductsMenu/ProductsMenu.tsx
@@ -5,7 +5,7 @@ export const ProductsMenu = () => {
    return (
       <aside className="baseTablet:flex-grow-1 sticky top-0 w-full baseTablet:h-fit baseTablet:max-w-[240px] baseTablet:flex-shrink-0 baseTablet:basis-[100%] baseTablet:py-2">
          <div className={'flex-start flex flex-col gap-y-2'}>
-            <MenuTitle className={''} />
+            <MenuTitle className={'hidden baseTablet:block'} />
             <MenuLinks />
          </div>
       </aside>
