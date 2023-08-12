cc678748ca2abea4d2eea003685a27f80fc86398
okanay <okanay@hotmail.com>
2023-08-12 20:16:23 +0300

    Landing Slider Animation Code Improve

--- src/components/Index-Page-Components/Landing-Section/LandingSlider.tsx
+++ src/components/Index-Page-Components/Landing-Section/LandingSlider.tsx
@@ -1,70 +1,58 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { useAnimate } from 'framer-motion'
 
 import Image1 from '../../../../public/images/LandingImages/SlideImages/SlideImageFirst.jpg'
 import Image2 from '../../../../public/images/LandingImages/SlideImages/SlideImageSecond.jpg'
 import Image3 from '../../../../public/images/LandingImages/SlideImages/SlideImageThird.jpg'
 import Image4 from '../../../../public/images/LandingImages/SlideImages/SlideImageFourth.jpg'
+const images = [Image1, Image2, Image3, Image4]
+
 import Image from 'next/image'
+import { AnimatePresence, motion, MotionConfig } from 'framer-motion'
 
 export const LandingSlider = () => {
-   //
-   //
-   //
-
    const [index, setIndex] = useState(0)
-   const [initial, setInitial] = useState(true)
-   const [duration, setDuration] = useState(0.4)
-   const [scope, animate] = useAnimate()
-
-   //
-   //
-   //
 
    useEffect(() => {
-      animate(scope.current, { x: index * -12.5 + '%' }, { ease: 'linear', duration: duration })
-   }, [index])
+      const imageIndexTimeout = setTimeout(() => {
+         setIndex(index + 1)
+      }, 5000)
 
-   //
-   //
-   //
-
-   useEffect(() => {
-      const nextIndex = () => {
-         if (index <= 3) {
-            setDuration(0.4)
-            setIndex(index + 1)
-         } else {
-            setDuration(0)
-            setIndex(0)
-         }
-      }
-      //
-      const timeout = index === 0 ? (initial ? 4000 : 250) : 4000
-      const animateTimeout = setTimeout(nextIndex, timeout)
-      //
-      setInitial(false)
-      //
       return () => {
-         clearTimeout(animateTimeout)
+         clearTimeout(imageIndexTimeout)
       }
    }, [index])
 
    return (
-      <div className="relative hidden h-[32rem] w-screen overflow-hidden baseTablet:block">
-         <div ref={scope} className="absolute z-20 flex h-full w-[800%] overflow-hidden">
-            <Image src={Image1} alt={'image-1'} priority className="h-full w-screen object-cover object-center" />
-            <Image src={Image2} alt={'image-2'} priority className="h-full w-screen object-cover object-center" />
-            <Image src={Image3} alt={'image-3'} priority className="h-full w-screen object-cover object-center" />
-            <Image src={Image4} alt={'image-4'} priority className="h-full w-screen object-cover object-center" />
-            <Image src={Image1} alt={'image-1-copy'} priority className="h-full w-screen object-cover object-center" />
-            <Image src={Image2} alt={'image-2-copy'} priority className="h-full w-screen object-cover object-center" />
-            <Image src={Image3} alt={'image-3-copy'} priority className="h-full w-screen object-cover object-center" />
-            <Image src={Image4} alt={'image-4-copy'} priority className="h-full w-screen object-cover object-center" />
+      <MotionConfig>
+         <div className="relative hidden h-[32rem] w-screen overflow-hidden baseTablet:block">
+            <div className="absolute left-0 top-0 z-30 z-[9] h-full w-full overflow-hidden bg-gradient-to-tr from-skin-theme-700 to-skin-theme-900/30">
+               <AnimatePresence custom={index}>
+                  <motion.div
+                     key={index}
+                     variants={imagesAnimationVariants}
+                     initial={'enter'}
+                     animate={'center'}
+                     exit={'exit'}
+                     custom={index}
+                     className={`absolute inset-0`}>
+                     <div className={'absolute inset-0'}>
+                        <Image src={images[index % 4]} alt={''} className={'h-full w-full object-cover'} />
+                     </div>
+                  </motion.div>
+               </AnimatePresence>
+            </div>
+            <div className="absolute left-0 top-0 z-30 z-[10] h-full w-full overflow-hidden bg-gradient-to-tr from-skin-theme-700 to-skin-theme-900/30"></div>
          </div>
-         <div className={'absolute left-0 top-0 z-30 h-full w-full bg-gradient-to-tr from-skin-theme-700/90 to-slate-400/30'} />
-      </div>
+      </MotionConfig>
    )
+}
+
+const imagesAnimationVariants = {
+   enter: (index: number) => ({
+      x: index === 0 ? '0%' : '100%',
+   }),
+   center: { x: '0%', transition: { duration: 2 } },
+   exit: { x: '-100%', transition: { duration: 2 } },
 }
