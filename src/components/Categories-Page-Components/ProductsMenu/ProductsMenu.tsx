69cd69b19934f63bb1dceaac691dbde5b813b637
okanay <okanay@hotmail.com>
2023-08-13 03:53:14 +0300

    Categories loading.tsx

--- src/components/Categories-Page-Components/ProductsMenu/ProductsMenu.tsx
+++ src/components/Categories-Page-Components/ProductsMenu/ProductsMenu.tsx
@@ -4,6 +4,8 @@ import { MenuLinks } from '@/components/Categories-Page-Components/ProductsMenu/
 import { MenuTitle } from '@/components/Categories-Page-Components/ProductsMenu/MenuTitle'
 
 export const ProductsMenu = () => {
+   console.log('render')
+
    return (
       <aside className="sticky top-0 z-[200] w-full baseTablet:h-fit baseTablet:max-w-[240px] baseTablet:flex-shrink-0 baseTablet:flex-grow baseTablet:basis-[100%] baseTablet:py-2">
          <div className={'flex-start flex flex-col gap-y-2'}>
