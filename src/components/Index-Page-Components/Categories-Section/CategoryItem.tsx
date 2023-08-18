3930152faca27afff8dc1d6f6093ee410e6242ef
okanay <okanay@hotmail.com>
2023-08-18 23:31:00 +0300

    as param

--- src/components/Index-Page-Components/Categories-Section/CategoryItem.tsx
+++ src/components/Index-Page-Components/Categories-Section/CategoryItem.tsx
@@ -18,11 +18,11 @@ export const CategoryItem = ({ category }: { category: Category }) => {
    }
 
    return (
-      <a
-         href={category.url}
+      <Link
+         href={'/[key]'}
+         as={`../${category.url}`}
          onClick={handleOnClick}
-         className="flex h-24 w-full flex-col items-center justify-start gap-1 rounded-lg border border-transparent py-2 text-center transition-colors duration-300 hover:border-skin-theme-50 hover:bg-skin-theme-50"
-      >
+         className="flex h-24 w-full flex-col items-center justify-start gap-1 rounded-lg border border-transparent py-2 text-center transition-colors duration-300 hover:border-skin-theme-50 hover:bg-skin-theme-50">
          <div className={'flex w-full flex-col items-center justify-start gap-2'}>
             <div className={'relative h-11 w-11 overflow-hidden rounded-lg border border-slate-300 bg-white'}>
                <ImageOptimization alt={{ tr: '', en: '' }} imageNormal={category.image} imageSmall={category.imageSmall} />
@@ -31,6 +31,6 @@ export const CategoryItem = ({ category }: { category: Category }) => {
                {t(category.languageCode)}
             </h4>
          </div>
-      </a>
+      </Link>
    )
 }
