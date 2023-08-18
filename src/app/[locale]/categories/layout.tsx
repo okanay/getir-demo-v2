f57ea25f317d7819e879e2615f1a497ae51a98ed
okanay <okanay@hotmail.com>
2023-08-19 02:53:30 +0300

    bg-white

--- src/app/[locale]/categories/layout.tsx
+++ src/app/[locale]/categories/layout.tsx
@@ -10,7 +10,7 @@ type TProps = {
 
 export default function CategoriesLayout({ children }: TProps) {
    return (
-      <main className={'h-full w-full'}>
+      <main className={'h-full w-full bg-white'}>
          <CategoriesHeader />
          {/*<CategoriesPromotions />*/}
          <ProductsContainer>
